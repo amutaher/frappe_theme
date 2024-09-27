59882a95e1537a6f3a4401c494a42da6811e1b71
Samar Raj <samar.suvaidyam@gmail.com>
2024-09-27 05:13:31 +0000

    doctypetble

--- frappe_theme/hooks.py
+++ frappe_theme/hooks.py
@@ -17,6 +17,7 @@ app_include_js = [
     "/assets/frappe_theme/js/mobile_view.js",
     "/assets/frappe_theme/js/workspace.js",
     "/assets/frappe_theme/js/sva_datatable.js",
+    "/assets/frappe_theme/js/doctype_table.js",
 ]
 # include js, css files in header of web template
 # web_include_css = "/assets/frappe_theme/css/frappe_theme.css"
