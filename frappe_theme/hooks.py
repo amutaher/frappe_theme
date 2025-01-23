b860267ab703869c46ac4edf42749daf4ec362fc
Samar Raj <samar.suvaidyam@gmail.com>
2025-01-23 11:03:27 +0000

    hooks testing

--- frappe_theme/hooks.py
+++ frappe_theme/hooks.py
@@ -26,7 +26,7 @@ app_include_js = [
     f"/assets/frappe_theme/js/mobile_view.js?ver={time.time()}",
     f"/assets/frappe_theme/js/workspace.js?ver={time.time()}",
     f"/assets/frappe_theme/js/date_utils.js?ver={time.time()}",
-    f"assets/frappe_theme/js/number_card.js?ver={time.time()}",
+    # f"assets/frappe_theme/js/number_card.js?ver={time.time()}",
     f"/assets/frappe_theme/js/sva_datatable.js?ver={time.time()}"
 ]
 extend_bootinfo = f"frappe_theme.boot.boot_theme"
