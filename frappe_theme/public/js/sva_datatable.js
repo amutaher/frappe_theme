bab51b1549f2e7f5d4ac3e72f0cc3e4e54d472df
Samar Raj <samar.suvaidyam@gmail.com>
2025-02-12 07:56:04 +0000

    code for disabling edit and delete for rows in final positive stage

--- frappe_theme/public/js/sva_datatable.js
+++ frappe_theme/public/js/sva_datatable.js
@@ -1009,7 +1009,7 @@ class SvaDataTable {
         });
     }
     createActionColumn(row, primaryKey) {
-
+        const positiveClosureState = this.workflow?.states?.find(s =>  s.custom_closure == "Positive");
         const dropdown = document.createElement('div');
         dropdown.classList.add('dropdown');
 
@@ -1043,14 +1043,30 @@ class SvaDataTable {
         // Edit and Delete Buttons
         if (!['1', '2'].includes(row.docstatus) && this.frm?.doc?.docstatus == 0) {
             if (this.permissions.includes('write')) {
-                appendDropdownOption('Edit', async () => {
-                    await this.createFormDialog(this.doctype, primaryKey, 'write');
-                });
+                if (positiveClosureState && row['workflow_state']){
+                    if((positiveClosureState.state != row['workflow_state'])){
+                        appendDropdownOption('Edit', async () => {
+                            await this.createFormDialog(this.doctype, primaryKey, 'write');
+                        });
+                    }
+                }else{
+                    appendDropdownOption('Edit', async () => {
+                        await this.createFormDialog(this.doctype, primaryKey, 'write');
+                    });
+                }
             }
             if (this.permissions.includes('delete')) {
-                appendDropdownOption('Delete', async () => {
-                    await this.deleteRecord(this.doctype, primaryKey);
-                });
+                if (positiveClosureState && row['workflow_state']){
+                    if((positiveClosureState.state != row['workflow_state'])){
+                        appendDropdownOption('Delete', async () => {
+                            await this.deleteRecord(this.doctype, primaryKey);
+                        });
+                    }
+                }else{
+                    appendDropdownOption('Delete', async () => {
+                        await this.deleteRecord(this.doctype, primaryKey);
+                    });
+                }
             }
         }
 
@@ -1257,14 +1273,14 @@ class SvaDataTable {
             },
             ...(fields ? fields : []),
         ];
+        let dialog;
         const workflowFormValue = await new Promise((resolve) => {
-            const dialog = new frappe.ui.Dialog({
+            dialog = new frappe.ui.Dialog({
                 title: "Confirm",
                 size: this.getDialogSize(popupFields),
                 fields: popupFields,
                 primary_action_label: "Proceed",
                 primary_action: (values) => {
-                    dialog.hide();
                     resolve(values);
                 },
                 secondary_action_label: "Cancel",
@@ -1283,6 +1299,7 @@ class SvaDataTable {
                 ...(workflowFormValue && workflowFormValue),
             };
             const response = await frappe.db.set_value(me.doctype, docname, updateFields);
+            dialog.hide();
             if (response?.exc) throw new Error("Update failed");
             const row = me.rows.find((r) => r.name === docname);
             row[me.workflow.workflow_state_field] = selected_state_info.next_state;
