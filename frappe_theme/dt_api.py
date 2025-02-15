3c330732814e0e13d53fa5d498738f3b15ad703b
Aniket <aniket@Anikets-MacBook-Air.local>
2025-02-15 16:17:35 +0530

    add custom link filter

---
+++ frappe_theme/dt_api.py
@@ -0,0 +1,25 @@
+import frappe
+
+@frappe.whitelist()
+def get_direct_connection_dts(dt):
+    standard_dts = frappe.get_list('DocField',filters=[
+        ['DocField', 'options', '=', dt],
+        ['DocField', 'parenttype', '=', "DocType"]
+    ],pluck='parent',ignore_permissions=True)
+    custom_dts = frappe.get_list('Custom Field',filters=[
+        ['Custom Field', 'options', '=', dt],
+    ],pluck='dt',ignore_permissions=True)
+    return standard_dts + custom_dts
+
+@frappe.whitelist()
+def get_direct_connection_fields(dt,link_dt):
+    standard_dt_fields = frappe.get_list('DocField',filters=[
+        ['DocField', 'options', '=', dt],
+        ['DocField', 'parenttype', '=', "DocType"],
+        ['DocField', 'parent', '=', link_dt]
+    ],fields=['label','fieldname'],ignore_permissions=True)
+    custom_dt_fields = frappe.get_list('Custom Field',filters=[
+        ['Custom Field', 'options', '=', dt],
+        ['Custom Field', 'dt', '=', link_dt]
+    ],fields=['label','fieldname'],ignore_permissions=True)
+    return standard_dt_fields + custom_dt_fields
