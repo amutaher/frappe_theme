2e39f7a2674fd7553c2e78f4e21c778d01c91510
Samar Raj <samar.suvaidyam@gmail.com>
2025-03-30 07:05:47 +0000

    code for sva_datatable settings

--- frappe_theme/public/js/sva_datatable.js
+++ frappe_theme/public/js/sva_datatable.js
@@ -1725,7 +1725,6 @@ class SvaDataTable {
                     console.log("Exception: while parsing extended_condition", error);
                 }
             }
-            console.log(this.connection,'this.connection/////')
             if (this.connection?.connection_type === 'Referenced') {
                 filters.push([this.doctype, this.connection.dt_reference_field, '=', this.frm.doc.doctype]);
                 filters.push([this.doctype, this.connection.dn_reference_field, '=', this.frm.doc.name]);
