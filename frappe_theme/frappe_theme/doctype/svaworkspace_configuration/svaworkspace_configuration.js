400dc2fef757062ed7f26db6b5d0415e60a953af
amreshyadavgit <ami92.raj@gmail.com>
2025-04-02 15:52:41 +0000

    added datatable feature in workspace

--- frappe_theme/frappe_theme/doctype/svaworkspace_configuration/svaworkspace_configuration.js
+++ frappe_theme/frappe_theme/doctype/svaworkspace_configuration/svaworkspace_configuration.js
@@ -7,6 +7,77 @@ frappe.ui.form.on("SVAWorkspace Configuration", {
 	},
 
 });
+const set_list_settings = async (frm, cdt, cdn) => {
+    let row = locals[cdt][cdn];
+    let dtmeta = await frappe.call({
+        method: 'frappe_theme.api.get_meta',
+        args: { doctype: ["Direct", "Unfiltered"].includes(row.connection_type) ? row.link_doctype : row.referenced_link_doctype ?? row.link_doctype }
+    });
+    new ListSettings({
+        doctype: ["Direct", "Unfiltered"].includes(row.connection_type) ? row.link_doctype : row.referenced_link_doctype ?? row.link_doctype,
+        meta: dtmeta.message,
+        settings: row,
+        dialog_primary_action: async (listview_settings) => {
+            frappe.model.set_value(cdt, cdn, "listview_settings", JSON.stringify(listview_settings));
+            frappe.show_alert({ message: __('Listview settings updated'), indicator: 'green' });
+        }
+    });
+}
+const set_crud_permissiions = (frm, cdt, cdn) => {
+    let row = locals[cdt][cdn];
+    let prev_permissions = JSON.parse(row.crud_permissions ?? '["read", "write", "create", "delete"]');
+    let fields = ["read", "write", "create", "delete"].map(p => {
+        return {
+            label: p[0].toUpperCase() + p.slice(1),
+            fieldname: p,
+            fieldtype: 'Check',
+            default: prev_permissions.includes(p) || p === "read",
+            read_only: p === "read",
+            onchange: function () {
+                const fieldname = this.df.fieldname;
+                const value = this.get_value();
+                if (value) {
+                    if (!prev_permissions.includes(fieldname)) {
+                        prev_permissions.push(fieldname);
+                    }
+                } else {
+                    prev_permissions = prev_permissions.filter(f => f !== fieldname);
+                }
+            }
+        }
+    });
+    let permissions_dialog = new frappe.ui.Dialog({
+        title: __('CRUD Permissions'),
+        fields: fields,
+        primary_action_label: __('Save'),
+        primary_action: async () => {
+            frappe.model.set_value(cdt, cdn, "crud_permissions", JSON.stringify(prev_permissions));
+            permissions_dialog.hide();
+        }
+    });
+    permissions_dialog.show();
+}
+
+frappe.ui.form.on('SVAWorkspace DT Child',{
+    async setup_list_settings(frm, cdt, cdn) {
+        await set_list_settings(frm, cdt, cdn);
+    },
+    async setup_crud_permissions(frm, cdt, cdn) {
+        set_crud_permissiions(frm, cdt, cdn);
+    },
+    form_render:async (frm, cdt, cdn) => {
+        let workspace = frm.doc.workspace;
+        if(workspace){
+            let res = await frappe.call('frappe_theme.wp_api.get_html_blocks',{workspace: workspace});
+            frm.cur_grid.grid_form.fields_dict.custom_html_block.get_query = () => {
+                return {
+                    filters: { 'name': ['IN', res.message] },
+                    limit_page_length: 100
+                }
+            }
+        }
+    }
+})
 frappe.ui.form.on("SVAWorkspace Heatmap Child", {
     form_render: async function(frm, cdt, cdn) {
         let workspace = frm.doc.workspace;
@@ -62,7 +133,7 @@ frappe.ui.form.on("SVAWorkspace Heatmap Child", {
                             });
                         }
                     });
-                    
+
                     frappe.model.set_value(cdt, cdn, "target_fields", JSON.stringify(target_fields));
                     dialog.clear();
                     dialog.hide();
--- frappe_theme/public/js/workspace.js
+++ frappe_theme/public/js/workspace.js
@@ -1,3 +1,21 @@
+const get_wrapper = (block_name)=>{
+    return new Promise((rslv, rjct)=>{
+        let index = 0;
+        let interval = setInterval(() => {
+            let wrapper = document.querySelector(`[custom_block_name="${block_name}"]`);
+            if (wrapper) {
+                rslv(wrapper)
+            }
+            index++;
+            if (wrapper || index == 20) {
+                clearInterval(interval);
+                if(index == 20 && !wrapper){
+                    rjct(false)
+                }
+            }
+        }, 1000);
+    });
+}
 if (frappe?.workspace_block.blocks?.custom_block) {
     frappe.workspace_block.blocks.custom_block = class SVACustomBlock extends frappe.workspace_block.blocks.custom_block {
         constructor(...args) {
@@ -6,29 +24,38 @@ if (frappe?.workspace_block.blocks?.custom_block) {
                 this.render_sva_custom_block()
             }
         }
-        render_sva_custom_block() {
+        async render_sva_custom_block() {
             let page_type = frappe.get_route()[0];
             if (page_type == "Workspaces") {
                 let workspace_name = frappe.get_route()[1];
                 let workspace_conf = frappe.boot?.sva_workspaces?.[workspace_name];
-                let blocks = [...workspace_conf?.heatmaps.map((heatmap) => {return {...heatmap,'type':"Heatmap"}}),]
+                let blocks = [
+                    ...workspace_conf?.heatmaps.map((heatmap) => {return {...heatmap,'type':"Heatmap"}}),
+                    ...workspace_conf?.tables.map((table) => {return {...table,'type':"SVATable"}})
+                ]
                 if(workspace_conf){
-                    let block = blocks.find(e => e.custom_block == this.data.custom_block_name);
-                    if(block && block.type == "Heatmap"){
-                        let index = 0;
-                        let interval = setInterval(() => {
-                            let wrapper = document.querySelector(`[custom_block_name="${block.custom_block}"]`);
-                            if (wrapper) {
-                                new Heatmap({
-                                    wrapper: $(wrapper),
-                                    ...block,
-                                });
-                            }
-                            index++;
-                            if (wrapper || index == 20) {
-                                clearInterval(interval);
-                            }
-                        }, 1000);
+                    let block = blocks.find(block => (block?.custom_block || block?.custom_html_block) == this.data.custom_block_name);
+                    let wrapper = await get_wrapper(block?.custom_block || block?.custom_html_block)
+
+                    if(block && wrapper){
+                        if(block.type == "Heatmap"){
+                            new Heatmap({
+                                wrapper: $(wrapper),
+                                ...block,
+                            })
+                        }else if (block.type == 'SVATable') {
+                            new SvaDataTable({
+                                label: block?.title,
+                                wrapper,
+                                doctype: block?.link_doctype,
+                                connection:block,
+                                options:{
+                                    serialNumberColumn: true,
+                                    editable: false,
+                                    style:{height:'237px'}
+                                }
+                            })
+                        }
                     }
                 }
             }
