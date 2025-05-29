578cf4404276b9dc666012d9e19af51e153aefe9
rsuvaidyam <rkrahul00011@gmail.com>
2025-05-29 18:40:36 +0530

    fixed

--- frappe_theme/api.py
+++ frappe_theme/api.py
@@ -48,14 +48,7 @@ def get_meta_fields(doctype):
 def get_permissions(doctype,_type='Direct'):
     permissions = []
     if _type == 'Report':
-        dt = frappe.db.exists('SVADatatable Configuration Child',{'link_report':doctype})
-        ws = frappe.db.exists('SVAWorkspace DT Child',{'link_report':doctype})
-        if dt:
-            permissions = frappe.db.get_value('SVADatatable Configuration Child',{'link_report':doctype},['crud_permissions'])
-        elif ws:
-            permissions = frappe.db.get_value('SVAWorkspace DT Child',{'link_report':doctype},['crud_permissions'])
-        else:
-            permissions = ['read']
+        permissions.append('read')
     else:
         if frappe.has_permission(doctype,'read'):
             permissions.append('read')
@@ -69,7 +62,6 @@ def get_permissions(doctype,_type='Direct'):
             permissions.append('submit')
         if frappe.has_permission(doctype,'cancel'):
             permissions.append('cancel')
-    print('Permissions:///////////////////////', permissions)
     return permissions
 
 @frappe.whitelist() 
