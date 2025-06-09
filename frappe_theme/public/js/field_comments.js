148979e2f072486632a7eaebba2ad12a4d1af71a
Aniket <aniket@Anikets-MacBook-Air.local>
2025-06-09 12:29:00 +0530

    add enable and disable featurs checkbox in frappe theme and also comment icon not visible on read_only field

--- frappe_theme/public/js/field_comments.js
+++ frappe_theme/public/js/field_comments.js
@@ -778,6 +778,11 @@ function setupFieldComments(frm) {
     if (!frm.is_new()) {
         if (!frm.doc || !frm.doc.doctype) return;
 
+        // Check if field comments are disabled globally
+        if (frappe.boot.my_theme && frappe.boot.my_theme.hide_fields_comment) {
+            return;
+        }
+
         // Check permissions first
         check_comment_permissions().then(permissions => {
             // Only proceed if user has read permission
@@ -896,7 +901,9 @@ function setupFieldComments(frm) {
                         Object.keys(frm.fields_dict).forEach(fieldname => {
                             const field = frm.fields_dict[fieldname];
                             if (!field || !field.df) return;
-                            if (['Section Break', 'Column Break', 'Tab Break', 'HTML', 'Button'].includes(field.df.fieldtype)) {
+
+                            // Skip if field is read-only or is a layout field
+                            if (field.df.read_only || ['Section Break', 'Column Break', 'Tab Break', 'HTML', 'Button'].includes(field.df.fieldtype)) {
                                 return;
                             }
 
@@ -907,7 +914,7 @@ function setupFieldComments(frm) {
                                     <div class="field-comment-icon" style="display: none; position: absolute; right: -30px; top: -2px; z-index: 10;">
                                         <button class="btn" style="padding: 2px 8px; position: relative;">
                                             <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" fill="currentColor" class="bi bi-chat" viewBox="0 0 16 16">
-                                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 11 11 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8 8 0 0 0 8 14c3.996 0 7-2.807 7-6s-3.004-6-7-6-7 2.808-7 6c0 1.468.617 2.83 1.678 3.894m-.493 3.905a22 22 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a10 10 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9 9 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105"></path>
+                                                <path d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"/>
                                             </svg>
                                             <span class="comment-count-badge" style="
                                                 position: absolute;
