3c8b9d3eca193aa61e19247572c7ef9c44a04fc1
abhirock74 <abhishek.suvaidyam@gmail.com>
2025-07-28 05:37:24 +0000

    changes as per copilot

--- frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
+++ frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
@@ -1,14 +1,22 @@
 // Copyright (c) 2025, Suvaidyam and contributors
 // For license information, please see license.txt
-monthly_dates = [
-    "",
-    "Start of the month",
-    "End of the month",
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-    11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-    21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
-]
-Quarterly_dates = ["", "Start of the month", "End of the month"]
+function setDayOptions(frm) {
+    monthly_dates = [
+        "",
+        "Start of the month",
+        "End of the month",
+        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+    ]
+    Quarterly_dates = ["", "Start of the month", "End of the month"]
+
+    if (frm.doc.frequency === "Quarterly") {
+        frm.set_df_property("day", "options", Quarterly_dates);
+    } else {
+        frm.set_df_property("day", "options", monthly_dates);
+    }
+}
 
 frappe.ui.form.on("SVA Task Planner", {
     refresh(frm) {
@@ -19,10 +27,3 @@ frappe.ui.form.on("SVA Task Planner", {
     }
 });
 
-function setDayOptions(frm) {
-    if (frm.doc.frequency === "Quarterly") {
-        frm.set_df_property("day", "options", Quarterly_dates);
-    } else {
-        frm.set_df_property("day", "options", monthly_dates);
-    }
-}
