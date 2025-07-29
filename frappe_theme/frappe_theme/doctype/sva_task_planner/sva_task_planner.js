5f185143bfec626fb8e667bc134eefb41ca217f4
abhirock74 <abhishek.suvaidyam@gmail.com>
2025-07-29 04:53:50 +0000

    changes as per reviewer request

--- frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
+++ frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
@@ -16,10 +16,10 @@ function setDayOptions(frm) {
         11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
         21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
     ]
-    Quarterly_dates = ["", "Start of the month", "End of the month"]
+    quarterly_dates = ["", "Start of the month", "End of the month"]
 
     if (frm.doc.frequency === "Quarterly") {
-        frm.set_df_property("day", "options", Quarterly_dates);
+        frm.set_df_property("day", "options", quarterly_dates);
     } else {
         frm.set_df_property("day", "options", monthly_dates);
     }
