92c5a8a820b4e526cea5eb2d5f87baae4a651aa5
abhirock74 <abhishek.suvaidyam@gmail.com>
2025-07-31 04:51:18 +0000

    cahnge the options as per review

--- frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
+++ frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
@@ -1,28 +1,34 @@
 // Copyright (c) 2025, Suvaidyam and contributors
 // For license information, please see license.txt
-function setDayOptions(frm) {
-    frm.set_query("parent_task_name", function () {
-        return {
-            filters: {
-                "is_group": 1
-            }
-        };
-    });
-    monthly_dates = [
-        "",
-        "Start of the month",
-        "End of the month",
-        1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
-        11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
-        21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
-    ]
-    quarterly_dates = ["", "Start of the month", "End of the month"]
 
-    if (frm.doc.frequency === "Quarterly") {
-        frm.set_df_property("day", "options", quarterly_dates);
-    } else {
-        frm.set_df_property("day", "options", monthly_dates);
-    }
+function setDayOptions(frm) {
+    const dayOptionsByFrequency = {
+        "Monthly": [
+            "", "Start of the month", "End of the month",
+            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
+            21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
+        ],
+        "Quarterly": [
+            "", "Start of the month", "End of the month"
+        ],
+        "Weekly": [
+            "", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
+        ],
+        "Fortnightly": [
+            "", "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14", "15"
+        ]
+    };
+
+    const options = dayOptionsByFrequency[frm.doc.frequency] || dayOptionsByFrequency["Monthly"];
+    frm.set_df_property("day", "options", options);
+
+    // Optional: filter for parent_task_name
+    frm.set_query("parent_task_name", () => ({
+        filters: {
+            is_group: 1
+        }
+    }));
 }
 
 frappe.ui.form.on("SVA Task Planner", {
@@ -33,4 +39,3 @@ frappe.ui.form.on("SVA Task Planner", {
         setDayOptions(frm);
     }
 });
-
