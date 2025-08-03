753002bf2934397ead6f1b2ea6e75255aa4f8e2e
abhirock74 <abhishek.suvaidyam@gmail.com>
2025-08-03 04:54:40 +0000

    change option for Quarterly

--- frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
+++ frappe_theme/frappe_theme/doctype/sva_task_planner/sva_task_planner.js
@@ -5,12 +5,12 @@ function set_day_options(frm) {
     const day_options_by_frequency = {
         "Monthly": [
             "Start of the month", "End of the month",
-            1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+            2, 3, 4, 5, 6, 7, 8, 9, 10,
             11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
             21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31
         ],
         "Quarterly": [
-            "Start of the month", "End of the month"
+            "Start of the Quarter", "End of the Quarter"
         ],
         "Weekly": [
             "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday"
